2f9e6ddaefeb8636b9cf9b88f83dfb8b7ca4fdc8
InJung Chung <mu29gl@gmail.com>
2016-05-18 00:12:16 +0900

    로그인 뷰

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,12 @@ class UsersController < ApplicationController
   get '/users' do
     render_template :index
   end
+
+  get '/users/new' do
+    render_template :new
+  end
+
+  get '/users/login' do
+    render_template :login
+  end
 end
