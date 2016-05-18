2e9f4d631d6bf5235122bb4ce9ac92554e161531
InJung Chung <mu29gl@gmail.com>
2016-05-18 16:30:47 +0900

    위키 편집하기, 랜덤 위키 찾아가기

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -1,6 +1,11 @@
 class WikisController < ApplicationController
-  get '/wikis/:title' do
-    @wiki = Wiki.where(title: params[:title]).order('created_at DESC').last
+  get '/wikis/show' do
+    title = URI.escape(Wiki.find(rand(Wiki.count) + 1).title)
+    redirect url("/wikis/show/#{title}")
+  end
+
+  get '/wikis/show/:title' do
+    @wiki = Wiki.where(title: params[:title]).order('created_at DESC').first
     render_template :show
   end
 
@@ -17,7 +22,28 @@ class WikisController < ApplicationController
 
     title = URI.escape(@wiki.title)
     if @wiki.save
-      redirect url("/users/wikis/#{title}")
+      redirect url("/wikis/show/#{title}")
+    else
+      @message = '문서 생성에 실패하였습니다.'
+      render_template :new
+    end
+  end
+
+  get '/wikis/edit/:title' do
+    @wiki = Wiki.where(title: params[:title]).order('created_at DESC').last
+    render_template :edit
+  end
+
+  post '/wikis/edit' do
+    redirect url('/users/login') unless logged_in?
+
+    @wiki = Wiki.create(title: params[:title],
+                        content: params[:content],
+                        user: current_user)
+
+    title = URI.escape(@wiki.title)
+    if @wiki.save
+      redirect url("/wikis/show/#{title}")
     else
       @message = '문서 생성에 실패하였습니다.'
       render_template :new
