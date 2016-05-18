99ae06afa21425a966464e9571a644e978addcc9
InJung Chung <mu29gl@gmail.com>
2016-05-19 02:37:25 +0900

    위키가 content 가져야지

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -1,5 +1,6 @@
 class Wiki < ActiveRecord::Base
   validates_presence_of :title
+  validates_presence_of :content
   validates_presence_of :user_id
   validates_uniqueness_of :version, scope: :title
 
